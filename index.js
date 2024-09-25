452832ae1ec3ab51668dacfaa072e89c311cc7b3
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-09-25 22:27:19 +0300

    Adds functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    const hqStreet =42;
+    return Math.abs(someValue - hqStreet);
+}
+
+function distanceFromHqInFeet(someValue){
+    const hqInFeet = 264;
+    return distanceFromHqInBlocks(someValue) * hqInFeet;
+}
+
+// console.log(distanceFromHqInFeet(50));
+function distanceTravelledInFeet(start, destination){
+    const distanceInBlocks = Math.abs(destination - start);
+    return distanceInBlocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination); 
+
+    if (distance <= 400) {
+        return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far'; 
+    }
+}
